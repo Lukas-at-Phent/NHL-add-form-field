e2c785b0e550e72039b7eab0fe5399572371b0d1
Lukas-at-Phent <110130469+Lukas-at-Phent@users.noreply.github.com>
2023-02-16 00:03:09 +0100

    Create button-add-input-field.js

---
+++ button-add-input-field.js
@@ -0,0 +1,53 @@
+const form = document.querySelector('Form-block-logic');
+const addButton = document.querySelector('.new-field-button');
+const outerWrapper = document.querySelector('.new-form-field-wrapper');
+const inputClasses = ['form-input-shadow', 'w-input'];
+const placeholders = ['Substans', 'Råvara', 'Mängd', '%DRI'];
+
+const createInputElement = (type, maxlength, placeholder) => {
+  const input = document.createElement('input');
+  input.setAttribute('type', type);
+  input.setAttribute('maxlength', maxlength);
+  input.placeholder = placeholder;
+  input.classList.add(...inputClasses);
+  return input;
+};
+
+let ingredientNumber = 2; // initialize the variable to 2
+
+addButton.addEventListener('click', function() {
+  const row = document.createElement('div');
+  row.classList.add('ingredient-row');
+
+  const inputs = [
+    createInputElement('text', '256', placeholders[0]),
+    createInputElement('text', '256', placeholders[1]),
+    createInputElement('text', '256', placeholders[2]),
+    createInputElement('text', '256', placeholders[3]),
+  ];
+
+  const inputWrappers = inputs.map((input) => {
+    const inputWrapper = document.createElement('div');
+    inputWrapper.classList.add('form-field-wrapper');
+    inputWrapper.appendChild(input);
+    return inputWrapper;
+  });
+
+  inputWrappers.forEach((inputWrapper) => {
+    row.appendChild(inputWrapper);
+  });
+
+  const ingrediensTextBlock = document.createElement('div');
+  ingrediensTextBlock.classList.add('ingrediens');
+  ingrediensTextBlock.innerText = 'Ingrediens ' + ingredientNumber; // update the text content
+
+  const TextBlockWrapper = document.createElement('div');
+  TextBlockWrapper.classList.add('ingredients-wrapper');
+
+  TextBlockWrapper.appendChild(ingrediensTextBlock)
+  row.appendChild(TextBlockWrapper);
+
+  outerWrapper.appendChild(row);
+
+  ingredientNumber++; // increment the variable
+});
