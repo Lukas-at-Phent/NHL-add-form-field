a80d59903ab567566d4bf9619039781a58fb9835
Lukas-at-Phent <110130469+Lukas-at-Phent@users.noreply.github.com>
2023-02-21 19:13:22 +0100

    Update Change-text-from-checkbox.js

--- Change-text-from-checkbox.js
+++ Change-text-from-checkbox.js
@@ -1,7 +1,6 @@
-
 function handleChange() {
   const checkbox2 = document.getElementById('checkbox-per-dos');
-  const textBlock = document.querySelector('.checkbox-target');
+  const textBlock = document.querySelector('.span-text-target');
 
   if (checkbox2.checked) {
     textBlock.innerText = 'Antal doser';
