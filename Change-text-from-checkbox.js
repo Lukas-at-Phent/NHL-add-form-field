f70bbf7e8a6f4af4b469b551930c65dc34d055c8
Lukas-at-Phent <110130469+Lukas-at-Phent@users.noreply.github.com>
2023-02-22 21:42:24 +0100

    small changes

--- Change-text-from-checkbox.js
+++ Change-text-from-checkbox.js
@@ -1,13 +1,11 @@
-function handleChange() {
-  const checkbox2 = document.getElementById('checkbox-per-dos');
-  const textBlock = document.querySelector('.span-text-target');
+const checkbox2 = document.getElementById('checkbox-per-dos');
+const textBlock = document.querySelector('.span-text-target');
 
-  if (checkbox2.checked) {
-    textBlock.innerText = 'Antal doser';
-  } else {
-    textBlock.innerText = 'Antal kapslar';
-  }
+function handleChange() {
+  textBlock.innerText = checkbox2.checked 
+                        ? 'Antal doser' 
+                        : 'Antal kapslar';
 }
 
-const checkbox2 = document.getElementById('checkbox-per-dos');
+
 checkbox2.addEventListener('click', handleChange);
